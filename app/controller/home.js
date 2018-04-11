779acb3a852a191f28e9a03799ecbd0020267764
yuran <335023379@qq.com>
2018-04-11 17:42:05 +0800

    init files
    init files

--- app/controller/home.js
+++ app/controller/home.js
@@ -3,8 +3,25 @@
 const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
-  async index() {
-    this.ctx.body = 'hi, egg';
+  async vote() {
+    await this.ctx.render('vote.html',{
+        title:'输入页'
+    })
+  }
+
+  async votepost() {
+    var request = await this.ctx.request.query;
+    var body = await this.ctx.request.body;
+    // var str = await this.ctx;
+    console.log(request);
+    console.log(body);
+    // console.log(str);
+    this.ctx.status = 200;
+
+  }
+
+  async result() {
+    this.ctx.body = 'hi, result';
   }
 }
 
--- app/router.js
+++ app/router.js
@@ -5,5 +5,8 @@
  */
 module.exports = app => {
   const { router, controller } = app;
-  router.get('/', controller.home.index);
+  router.get('/', controller.home.vote);
+  router.get('/vote', controller.home.vote);
+  router.post('/vote', controller.home.votepost);
+  router.get('/result', controller.home.result);
 };
